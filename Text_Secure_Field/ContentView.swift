ef83056478726535357c7b7ebfa275792202b7a0
sudoswift <sudoswift@gmail.com>
2021-05-24 00:29:27 +0900

    Initial Commit

---
+++ Text_Secure_Field/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Text_Secure_Field
+//
+//  Created by izvern on 2021/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Text_Secure_Field/Text_Secure_FieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text_Secure_FieldApp.swift
+//  Text_Secure_Field
+//
+//  Created by izvern on 2021/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Text_Secure_FieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
