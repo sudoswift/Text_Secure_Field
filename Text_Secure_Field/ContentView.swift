ef91914e1bfa72b0cfa440500fb1e2d70d221c23
sudoswift <sudoswift@gmail.com>
2021-05-24 01:09:57 +0900

    조건문과 isEmpty 를 통해서 각각 textfield와 securefield 에 String을 입력하면 Button이 나오게 바꾸는 기능을 추가함

--- Text_Secure_Field/ContentView.swift
+++ Text_Secure_Field/ContentView.swift
@@ -13,20 +13,43 @@ struct ContentView: View {
     var body: some View {
         VStack{
             HStack{
-                //TextField("placeholder 자리", text: Binding<String>)
-                TextField("아이디를 입력해주세요.", text: $name) // $ 달러 사인 필수 입력
-                    .disableAutocorrection(true)
-                    .textCase(.none)
-                Button(action: {
-                    name=""
-                }, label: {
-                    Image(systemName: "delete.left.fill")
-                        .foregroundColor(.black)
-                })
+                if !name.isEmpty{
+                    //TextField("placeholder 자리", text: Binding<String>)
+                    TextField("아이디를 입력해주세요.", text: $name) // $ 달러 사인 필수 입력
+                        .disableAutocorrection(true)
+                        .textCase(.none)
+                    Button(action: {
+                        name = ""
+                    }, label: {
+                        Image(systemName: "delete.left.fill")
+                            .foregroundColor(.black)
+                    })
+                } else {
+                    
+                    TextField("아이디를 입력해주세요.", text: $name)
+                        .disableAutocorrection(true)
+                        .textCase(.none)
+                }
+
             }
-            SecureField("비밀번호를 입력해주세요.", text: $password)
-                .disableAutocorrection(true)
-                .textCase(.none)
+                HStack{
+                    if !password.isEmpty{
+                        SecureField("비밀번호를 입력해주세요.", text: $password)
+                            .disableAutocorrection(true)
+                            .textCase(.none)
+                        Button(action: {
+                            password = ""
+                        }, label: {
+                            Image(systemName: "delete.left.fill")
+                                .foregroundColor(.black)
+                        })
+                    } else {
+                        SecureField("비밀번호를 입력해주세요.", text: $password)
+                            .disableAutocorrection(true)
+                            .textCase(.none)
+                    }
+                }
+
             Text("입력한 아이디 : \(name)")
         }.textFieldStyle(RoundedBorderTextFieldStyle())
         .padding(.horizontal, 50)
