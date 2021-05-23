9ec4ad0e5d495e36d24931a15bb177f374356b32
sudoswift <sudoswift@gmail.com>
2021-05-24 00:47:20 +0900

    add SecureField, disableAutocorrection and textCase

--- Text_Secure_Field/ContentView.swift
+++ Text_Secure_Field/ContentView.swift
@@ -8,10 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name = ""
+    @State private var name: String = ""
+    @State private var password: String = ""
     var body: some View {
         Form{
-            TextField("아이디", text: $name) // $ 달러 사인 필수 입력
+            //TextField("placeholder 자리", text: Binding<String>)
+            TextField("아이디를 입력해주세요.", text: $name) // $ 달러 사인 필수 입력
+                .disableAutocorrection(true)
+                .textCase(.none)
+            SecureField("비밀번호를 입력해주세요.", text: $password)
+                .disableAutocorrection(true)
+                .textCase(.none)
             Text("입력한 아이디는 \(name) 입니다.")
         }
     }
